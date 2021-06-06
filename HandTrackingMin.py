85ea512c4eb3b03cef773fb8f725d677a90e9d47
Avinash Batchala <a.batchala@vikes.csuohio.edu>
2021-06-05 20:36:39 -0400

    added values of cx and cy

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -20,6 +20,15 @@ while True:
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                #print(id,lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id, cx, cy)
+                if id == 0:
+                    cv2.circle(img, (cx,cy), 15, (55, 43, 234), cv2.FILLED)
+
+
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
     cTime = time.time()
@@ -27,6 +36,6 @@ while True:
     pTime = cTime
 
     cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_COMPLEX, 3,
-    (255, 0, 255), 3)
+    (255, 45, 255), 3)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
